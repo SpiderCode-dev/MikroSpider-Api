a0d5cf8021a1701bcac7d52c541bcf664a602b9a
rojoarba <rogerarmas185@gmail.com>
2023-05-10 21:06:27 -0500

    Create conexion_api-mikrotik.py
    Script para verificar conexión al mikrotik via ip pública y api

---
+++ scripts/conexion_api-mikrotik.py
@@ -0,0 +1,16 @@
+import routeros_api
+
+##FUNCION DE VERIFICACIÓN DE CONEXIÓN AL MIKROTIK VIA API
+# PARAMETROS
+# ip_host: Es la ip pública del router mikrotik
+# user: nombre de usuario previamente creado en el router con los permisos requeridos
+# pwd: contraseña del usuario
+# port: puerto API, este valor por lo general es 8728, el mismo que es habilitado previamente
+def test_conexion(ip_host, user, pwd, port):
+    try: # Estructura try para controlar el error si no se establece la conexión
+        connection = routeros_api.RouterOsApiPool(ip_host, username=user, password=pwd, port=port, plaintext_login=True)
+        api = connection.get_api()
+        status = "La conexión via api se ha realizado exitosamente."
+    except routeros_api.exceptions.RouterOsApiConnectionError:
+        status = "No se ha logrado conectar via api, revisar la información ingresada por favor."
+    return status
