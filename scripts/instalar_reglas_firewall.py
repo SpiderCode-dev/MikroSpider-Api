4d623555e99bf05e31dc355e76560efd305eac80
rojoarba <rogerarmas185@gmail.com>
2023-05-15 21:35:58 -0500

    Update instalar_reglas_firewall.py
    Habilita el web proxy y el bloquea el puerto 999

--- scripts/instalar_reglas_firewall.py
+++ scripts/instalar_reglas_firewall.py
@@ -12,13 +12,15 @@ def crear_reglas(ip_host, user, pwd, port, interfaz):
     try:
         connection = routeros_api.RouterOsApiPool(ip_host, username=user, password=pwd, port=port, plaintext_login=True)
         api = connection.get_api()
-        # CREACION DE REGLA FIREWALL PARA PERMITIR TRAFICO DE CLIENTES ACTIVOS
-        filter_rules = api.get_resource("/ip/firewall/filter")
-        filter_rules.add(chain="FORWARD", action="accept", src_address_list="ips_autorizadas_mikrospider",
-                         comment="Permitir trafico IPs Autorizadas")
         # CREACION DE NOMBRE DE LA LISTA DE INTERFACE CON SALIDA A WAN
         interface_name = api.get_resource("/interface/list")
         interface_name.add(name="Lista_WAN")
+        # CREACION DE REGLA FIREWALL PARA PERMITIR TRAFICO DE CLIENTES ACTIVOS Y BLOQUEAR PUERTO 999
+        filter_rules = api.get_resource("/ip/firewall/filter")
+        filter_rules.add(chain="FORWARD", action="accept", src_address_list="ips_autorizadas_mikrospider",
+                         comment="Permitir trafico IPs Autorizadas")
+        filter_rules.add(chain="INPUT", action="drop", comment="Bloquear puerto WebProxy desde WANs", dst_port="999", in_interface_list="Lista_WAN",
+                         protocol="tcp")
         # CREACION DE UN MIEMBRO/MIEMBROS EN LA LISTA DE INTERFACE CON LA SALIDA A WAN
         interface_list = api.get_resource("/interface/list/member")
         for i in interfaz:
@@ -29,6 +31,9 @@ def crear_reglas(ip_host, user, pwd, port, interfaz):
                 src_address_list="ips_sin_servicio", action="redirect", to_ports="999", comment="Mikrospider - Suspension de clientes (TCP)")
         nat.add(chain="dstnat", protocol="udp", dst_port="!8291,53", in_interface_list="!Lista_WAN",
                 src_address_list="ips_sin_servicio", action="redirect", to_ports="999", comment="Mikrospider - Suspension de clientes (UDP)")
+        # HABILITAR EL WEB PROXY
+        proxy = api.get_resource("/ip/proxy")
+        proxy.set(enabled="yes", max_fresh_time="10s", port="999")
         status = "Reglas para la suspensión del servicio creadas exitosamente."
     except routeros_api.exceptions.RouterOsApiConnectionError:
         status = "No se ha podido realizar los cambios debido a un error de conexión."
