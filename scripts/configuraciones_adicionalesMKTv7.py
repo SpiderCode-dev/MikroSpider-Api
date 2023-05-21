924105b94462f1bdb9e1b84e93134adc611cf751
rojoarba <rogerarmas185@gmail.com>
2023-05-21 10:00:42 -0500

    Update configuraciones_adicionalesMKTv7.py
    Actualización de información

--- scripts/configuraciones_adicionalesMKTv7.py
+++ scripts/configuraciones_adicionalesMKTv7.py
@@ -1,12 +1,12 @@
 import routeros_api
 
-##FUNCION PARA REALIZAR CONFIGURACIONES ADICIONALES AL MIKROTIK
+##FUNCION PARA REALIZAR CONFIGURACIONES ADICIONALES AL MIKROTIK v7
 # ip_host: Es la ip pública del router mikrotik
 # user: nombre de usuario previamente creado en el router con los permisos requeridos
 # pwd: contraseña del usuario
 # port: puerto API, este valor por lo general es 8728, el mismo que es habilitado previamente
 # El formato de los datos a ingresar son tipo String, a excepción del puerto API que puede ser ingresado como número entero
-def config_adicional(ip_host, user, pwd, port):
+def config_adicionalv7(ip_host, user, pwd, port):
     try: # Estructura try para controlar el error si no se establece la conexión
         connection = routeros_api.RouterOsApiPool(ip_host, username=user, password=pwd, port=port, plaintext_login=True)
         api = connection.get_api()
@@ -21,5 +21,5 @@ def config_adicional(ip_host, user, pwd, port):
         dns = api.get_resource("/ip/dns")
         dns.set(servers="8.8.8.8,8.8.4.4")
     except routeros_api.exceptions.RouterOsApiConnectionError:
-        status = "No se ha logrado conectar via api, revisar la información ingresada por favor."
+        status = "No se ha realizado los cambios, verificar la conexión con el router via api por favor."
     return status
